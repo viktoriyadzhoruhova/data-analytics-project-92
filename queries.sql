b1b3214abfe5a7cc9097080b73449fa891d3badb
viktoriyadzhoruhova <142666685+viktoriyadzhoruhova@users.noreply.github.com>
2025-03-10 14:53:29 +0500

    Update queries2.sql

--- queries.sql
+++ queries.sql
@@ -42,4 +42,43 @@ inner join employees e on s.sales_person_id = e.employee_id
 inner join products p on s.product_id = p.product_id
 group by seller, day_of_week, TO_CHAR(s.sale_date, 'ID')
 order by seller, TO_CHAR(s.sale_date, 'ID') ASC;
-	
+
+-- Customers groups by age
+select 
+    case 
+        when age between 16 and 25 then '16-25'
+        when age between 26 and 40 then '26-40'
+        else '40+' 
+    end as age_category,
+    COUNT(*) as age_count
+from customers
+group by age_category
+order by age_category;
+
+-- Customers and income by months
+select 
+    TO_CHAR(s.sale_date, 'YYYY-MM') as selling_month,
+    COUNT(DISTINCT s.customer_id) as total_customers,
+    SUM(p.price * s.quantity) as income
+from sales s
+inner join products p on s.product_id = p.product_id
+group by selling_month
+order by selling_month;
+
+-- Customers whose first purchase was for the promotion
+with first_sale as (
+    select 
+        c.first_name || ' ' || c.last_name AS customer,
+        s.sale_date,
+        e.first_name || ' ' || e.last_name AS seller,
+        ROW_NUMBER() OVER (partition by c.customer_id order by s.sale_date) as row_number
+    from sales s
+    inner join customers c on s.customer_id = c.customer_id
+    inner join employees e on s.sales_person_id = e.employee_id
+    inner join products p on s.product_id = p.product_id
+    where p.price = 0
+)
+select customer, sale_date, seller
+from first_sale
+where row_number = 1
+order by customer;
